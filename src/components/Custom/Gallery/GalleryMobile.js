d237e55c37967c09c43bee015b8972890c960560
Mohamed Virji <mvirji@brrmedia.co.uk>
2023-10-04 04:55:03 +0100

    youtube modal for gallery

--- src/components/Custom/Gallery/GalleryMobile.js
+++ src/components/Custom/Gallery/GalleryMobile.js
@@ -1,9 +1,56 @@
 
 // @mui material components
+import { useState } from "react";
+import YouTube from 'react-youtube';
+
+// @mui material components
 import Grid from "@mui/material/Grid";
+import Modal from "@mui/material/Modal";
+import Divider from "@mui/material/Divider";
+import Slide from "@mui/material/Slide";
+
+import CloseIcon from "@mui/icons-material/Close";
+
+// Template components
+import MKBox from "components/MKBox";
+import MKBadge from "components/MKBadge";
+import MKTypography from "components/MKTypography";
+
 import { dark } from "@mui/material/styles/createPalette";
 
 function GalleryMobile() {
+  const [showOne, setShowOne] = useState(false);
+  const toggleModalOne = () => setShowOne(!showOne);
+
+  const [showTwo, setShowTwo] = useState(false);
+  const toggleModalTwo= () => setShowTwo(!showTwo);
+
+  const [showThree, setShowThree] = useState(false);
+  const toggleModalThree = () => setShowThree(!showThree);
+
+  const [showFour, setShowFour] = useState(false);
+  const toggleModalFour = () => setShowFour(!showFour);
+
+  const [showFive, setShowFive] = useState(false);
+  const toggleModalFive = () => setShowFive(!showFive);
+
+  const [showSix, setShowSix] = useState(false);
+  const toggleModalSix = () => setShowSix(!showSix);
+
+  const [showSeven, setShowSeven] = useState(false);
+  const toggleModalSeven = () => setShowSeven(!showSeven);
+
+  const [showEight, setShowEight] = useState(false);
+  const toggleModalEight = () => setShowEight(!showEight);
+
+  const opts = {
+    height: '300',
+    width: '400',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }}
+
   const imagesPrefix =
   "https://d17kgkt7t2bpo9.cloudfront.net/gallery";
   return (
@@ -16,18 +63,13 @@ function GalleryMobile() {
           {/* Portfolio items */}
           <Grid item xs={12} md={2}
             className="portfolio-item"
+            sx={{ cursor: "pointer" }} 
+            onClick={toggleModalOne}
           >
              <div className="portfolio-meta">
-          <a href="#" className="portfolio-like">
-            <i className="ion-ios-heart-outline"></i>
-          </a>
           <time>CREATIVE</time>
-          <a href="/creative" className="portfolio-item-more">
-            <i className="ion-ios-arrow-right"></i>
-          </a>
         </div>
         <header className="portfolio-header">
-          <a href="/creative">
             <figure className="portfolio-img">
               <img
                 src={`${imagesPrefix}/1.png`}
@@ -35,107 +77,254 @@ function GalleryMobile() {
                 className="featured-image"
               />
             </figure>
-          </a>
         </header>
           </Grid>
-          <Grid item xs={12} md={4} className="portfolio-item width-2x" >
+          <Modal open={showOne} onClose={toggleModalOne} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showOne} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">Pearl Yacht 72</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalOne} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="VgkZJDTQL8s" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+          <Grid item xs={12} md={4} className="portfolio-item width-2x" sx={{ cursor: "pointer" }} onClick={toggleModalTwo} >
           <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>EVENTS</time>
-<a href="/events" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/events">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/2.png`} alt="portfolio item 2" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={3} className="portfolio-item dark" >
+<Modal open={showTwo} onClose={toggleModalTwo} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showTwo} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">BIMA</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalTwo} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="QQoLefOqDoQ" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={3} className="portfolio-item dark" sx={{ cursor: "pointer" }} onClick={toggleModalThree}>
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>BRAND VIDEOS</time>
-<a href="/brand" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/brand">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/3.png`} alt="portfolio item 3" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={3} className="portfolio-item dark" >
+<Modal open={showThree} onClose={toggleModalThree} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showThree} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">TotalEnergies</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalThree} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="egU-X3ha7Q8" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={3} className="portfolio-item dark" sx={{ cursor: "pointer" }} onClick={toggleModalFour}>
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>TALKING HEADS</time>
-<a href="/talking-heads" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/talking-heads">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/9.png`} alt="portfolio item 4" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={3} className="portfolio-item" >
+<Modal open={showFour} onClose={toggleModalFour} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showFour} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">NJOY</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalFour} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="ptJifXxau_s" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={3} className="portfolio-item" sx={{ cursor: "pointer" }} onClick={toggleModalFive} >
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>PODCAST</time>
-<a href="/podcast" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/podcast">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/5-1.png`} alt="portfolio item 5" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={3} className="portfolio-item dark" >
+<Modal open={showFive} onClose={toggleModalFive} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showFive} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">LBPC</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalFive} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="tnFy7ns2HY8" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={3} className="portfolio-item dark" sx={{ cursor: "pointer" }} onClick={toggleModalSix} >
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>2D ANIMATION</time>
-<a href="/animation/2D" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/animation/2D">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/6.png`} alt="portfolio item 6" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={4} className="portfolio-item width-2x dark" >
+<Modal open={showSix} onClose={toggleModalSix} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showSix} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">The Change Foundation</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalSix} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="aX1pX7kQdqM" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={4} className="portfolio-item width-2x dark" sx={{ cursor: "pointer" }} onClick={toggleModalSeven} >
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>EVENTS</time>
-<a href="/events" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/events">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/7.png`} alt="portfolio item 7" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
-<Grid item xs={12} md={4} className="portfolio-item width-2x dark" >
+<Modal open={showSeven} onClose={toggleModalSeven} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showSeven} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">Cosun Beet</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalSeven} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="peRrOxSpmCY" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
+<Grid item xs={12} md={4} className="portfolio-item width-2x dark" sx={{ cursor: "pointer" }} onClick={toggleModalEight}  >
 <div className="portfolio-meta">
-<a href="#" className="portfolio-like"><i className="ion-ios-heart-outline"></i></a>
 <time>3D ANIMATION</time>
-<a href="/animation/3D" className="portfolio-item-more"><i className="ion-ios-arrow-right"></i></a>
 </div>
 <header className="portfolio-header">
-<a href="/animation/3D">
   <figure className="portfolio-img">
     <img src={`${imagesPrefix}/8.png`} alt="portfolio item 6" className="featured-image" />
   </figure>
-</a>
 </header>
 </Grid>
+<Modal open={showEight} onClose={toggleModalEight} sx={{ display: "grid", placeItems: "center" }}>
+          <Slide direction="down" in={showEight} timeout={500}>
+            <MKBox
+              position="relative"
+              width="400px"
+              height="300px"
+              display="flex"
+              flexDirection="column"
+              borderRadius="xl"
+              bgColor="#212529"
+              shadow="xl"
+            >
+              <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                <MKTypography variant="h5">3D Pearl Yachts</MKTypography>
+                <CloseIcon fontSize="medium" sx={{ cursor: "pointer", color:"#EC407A" }} onClick={toggleModalEight} />
+              </MKBox>
+              <Divider sx={{ my: 0 }} />
+              <YouTube videoId="KmIWUySR2k0" opts={opts} />
+              <Divider sx={{ my: 0 }} />
+            </MKBox>
+          </Slide>
+        </Modal>
           {/* <Grid
             item
             className="portfolio-item portfolio-more-items"
